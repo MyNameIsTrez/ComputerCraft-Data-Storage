eb0adffbfb599d3e4311861c4353c07b940d0120
MyNameIsTrez <welfje@gmail.com>
2020-07-13 19:34:52 +0200

    Fixed ComputerCraft code for sending HTTP POSTs, which means this code can be cleaned up a lot.

--- extra/ascii-compression/process-ascii-info.js
+++ extra/ascii-compression/process-ascii-info.js
@@ -4,31 +4,38 @@ const bodyParser = require("body-parser");
 const Datastore = require("nedb");
 
 const app = express();
-// Might want to remove "0.0.0.0", because I don't know if it does anything atm,
+// TODO: Might want to remove "0.0.0.0", because I don't know if it does anything atm,
 // and because it might be a security risk.
 app.listen(3000, "0.0.0.0", () => {
 	console.log("Listening at port 3000");
 });
 
 // ComputerCraft versions below 1.63 don't support custom headers,
-// so the content-type of those is always 'application/x-www-form-urlencoded'
+// so the content-type of those is always 'application/x-www-form-urlencoded'.
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/add", (request, response) => {
 	const info = repairMangledInfo(request.body);
-	if (checkInfoFormat(info)) {
-
+	const format = checkInfoFormat(info);
+	if (format === true) {
+		for (entry of info.entries) {
+			dbInsertEntry(entry, (id) => {
+				createAsciiFolder(id);
+				renderAscii(entry, id);
+			});
+		}
+	} else {
+		createError(format);
 	}
 	// ComputerCraft v1.33 doesn't feature http.post returning anything,
-	// so don't bother sending anything back
+	// so don't bother sending anything back.
 	response.end();
 });
 
 function repairMangledInfo(mangledInfo) {
-	const key1 = getMangledKey(mangledInfo);
-	const value = mangledInfo[key1];
-	const key2 = getMangledKey(value);
-	return JSON.parse(key1 + "[" + key2 + "]}]}");
+	const key = getMangledKey(mangledInfo);
+	const value = mangledInfo[key]
+	return JSON.parse(value);
 }
 
 function getMangledKey(mangledObj) {
@@ -36,38 +43,35 @@ function getMangledKey(mangledObj) {
 }
 
 function checkInfoFormat(info) {
+	if (info.password != "MyNameIsTrez") {
+		return "Wrong password";
+	}
 	// TODO: Check if the incoming info is in the correct format.
-	// 1. All fields filled with the correct types and 2. URL(s) exist.
-	// It'd be nice if ComputerCraft would send a GET right after POSTing the info, to an 'error' server page,
-	// for the purpose of seeing what the reason was that this function has rejected its sent info.
-	console.log("info:");
-	console.log(info);
+	// 1. The correct password was used, 2. All fields filled with the correct types and 3. All URLs exist.
+	return true;
 }
 
 const db = new Datastore({
 	filename: "ascii-info.db", autoload: true
 });
 
-// processPOST(JSON.parse(fs.readFileSync("input.txt", "utf8")));
-
-function processPOST(info) {
-	dbInsertInfo(info, (id) => {
-		createAsciiFolder(id);
-		renderAsciiPython(info, id);
-	});
-}
-
 function createAsciiFolder(id) {
 	fs.mkdirSync(`ascii-frames/${id}`);
 }
 
-function dbInsertInfo(info, callback) {
-	db.insert(info, (err, inserted) => {
+function dbInsertEntry(entry, callback) {
+	db.insert(entry, (err, inserted) => {
 		if (err) throw err;
 		callback(inserted._id);
 	});
 }
 
-function renderAsciiPython() {
-	// Add code that calls Python ascii rendering function here
+function renderAscii() {
+	// TODO: Add code that calls the Python ASCII rendering function here.
+}
+
+function createError(err) {
+	// TODO: It'd be nice if ComputerCraft would send a GET right after POSTing the info, to an 'error' server page,
+	// for the purpose of seeing what the reason was that this function has rejected its sent info.
+	console.log(err);
 }
