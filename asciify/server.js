a169b3145a356b04699cad3aaf9845923ec7c781
MyNameIsTrez <welfje@gmail.com>
2020-07-18 17:56:46 +0200

    Removed semicolons.

--- asciify/server.js
+++ asciify/server.js
@@ -1,54 +1,54 @@
-const fs = require("fs");
-const express = require("express");
-const bodyParser = require("body-parser");
-const Datastore = require("nedb");
-const { spawn } = require("child_process");
+const fs = require("fs")
+const express = require("express")
+const bodyParser = require("body-parser")
+const Datastore = require("nedb")
+const { spawn } = require("child_process")
 
-const app = express();
+const app = express()
 // TODO: Might want to remove "0.0.0.0", because I don't know if it does anything atm,
 // and because it might be a security risk.
 app.listen(3000, "0.0.0.0", () => {
-	console.log("Listening at port 3000");
-});
+	console.log("Listening at port 3000")
+})
 
 // ComputerCraft versions below 1.63 don't support custom headers,
 // so the content-type of those is always 'application/x-www-form-urlencoded'.
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const db = new Datastore({
 	filename: "ascii-info.db", autoload: true
-});
+})
 
 app.post("/add", async (req, res) => {
-	const info = repairMangledInfo(req.body);
-	const format = checkInfoFormat(info);
+	const info = repairMangledInfo(req.body)
+	const format = checkInfoFormat(info)
 	if (format === true) {
-		const entriesWithVariationIDs = await dbAddVariations(info.entries);
-		renderAscii(entriesWithVariationIDs);
+		const entriesWithVariationIDs = await dbAddVariations(info.entries)
+		renderAscii(entriesWithVariationIDs)
 	} else {
-		createError(format);
+		createError(format)
 	}
 	// ComputerCraft v1.33 doesn't feature http.post returning anything, so don't bother replying back.
-	res.end();
-});
+	res.end()
+})
 
 function repairMangledInfo(mangledInfo) {
-	const key = getMangledKey(mangledInfo);
+	const key = getMangledKey(mangledInfo)
 	const value = mangledInfo[key]
-	return JSON.parse(value);
+	return JSON.parse(value)
 }
 
 function getMangledKey(mangledObj) {
-	return Object.keys(mangledObj)[0];
+	return Object.keys(mangledObj)[0]
 }
 
 function checkInfoFormat(info) {
 	if (info.password != "MyNameIsTrez") {
-		return "Wrong password";
+		return "Wrong password"
 	}
 	// TODO: Check if the incoming info is in the correct format.
 	// 1. The correct password was used, 2. All fields filled with the correct types and 3. All URLs exist.
-	return true;
+	return true
 }
 
 async function dbAddVariations(entries) {
@@ -62,60 +62,60 @@ async function dbAddVariations(entries) {
 				"palette": variation.palette,
 				"width": variation.width,
 				"height": variation.height,
-			};
-			variation.id = await dbInsertVariation(variationInfo);
+			}
+			variation.id = await dbInsertVariation(variationInfo)
 		}
 	}
-	return entries; // TODO: Can this line be removed?
+	return entries // TODO: Can this line be removed?
 }
 
 async function dbInsertVariation(variation) {
 	return new Promise(function (resolve, reject) {
 		db.insert(variation, (err, inserted) => {
-			if (err) reject(err);
-			else resolve(inserted._id);
-		});
-	});
+			if (err) reject(err)
+			else resolve(inserted._id)
+		})
+	})
 }
 
 function renderAscii(entries) {
-	// console.log(JSON.stringify(entries, undefined, 2));
-	const py = spawn("python", ["python/render.py"]);
+	// console.log(JSON.stringify(entries, undefined, 2))
+	const py = spawn("python", ["python/render.py"])
 
 	// Prints whatever Python has attempted to print
 	py.stderr.on("data", (data) => {
-		console.error(`stderr: ${data}`);
-	});
+		console.error(`stderr: ${data}`)
+	})
 
 	py.stdout.on("data", (buffer) => {
-		const input = buffer.toString();
+		const input = buffer.toString()
 		// Important objects are sent through python's print(), other information is written to output.txt.
 		try {
-			dbAppendInfo(JSON.parse(input));
+			dbAppendInfo(JSON.parse(input))
 		} catch (error) {
-			console.log(error);
-			console.log(input);
+			console.log(error)
+			console.log(input)
 		}
-	});
+	})
 
-	py.stdin.write(JSON.stringify(entries));
-	py.stdin.end(); // TODO: Necessary?
+	py.stdin.write(JSON.stringify(entries))
+	py.stdin.end() // TODO: Necessary?
 }
 
 function dbAppendInfo(extraVariationsInfo) {
 	for (const [id, info] of Object.entries(extraVariationsInfo)) {
 		db.update({ _id: id }, {
 			$set: { "frame_files_count": info.frame_files_count, "frame_count": info.frame_count }
-		}, {}, function () { });
+		}, {}, function () { })
 	}
 }
 
 function createError(err) {
-	// TODO: It'd be nice if ComputerCraft would send a GET right after POSTing the info, to an 'error' server page,
+	// TODO: It'd be nice if ComputerCraft would send a GET right after POSTing the info, to an '/error' server endpoint,
 	// for the purpose of seeing what the reason was that this function has rejected its sent info.
-	console.log(err);
+	console.log(err)
 }
 
-app.get("/get-ascii-info", async (req, res) => { db.find({}, function (err, docs) { res.send(docs); }); });
+app.get("/get-ascii-info", async (req, res) => { db.find({}, function (err, docs) { res.send(docs) }) })
 
-app.get('/get-output', (req, res) => res.download('output.txt'))
+app.get("/get-output", (req, res) => res.download("output.txt"))
