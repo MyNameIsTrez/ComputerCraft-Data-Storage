4b231cc22152fac35ad07de9732ca04a28732b93
MyNameIsTrez <welfje@gmail.com>
2020-08-30 00:05:33 +0200

    Debugging.

--- asciify/python/utils/processing.py
+++ asciify/python/utils/processing.py
@@ -201,9 +201,13 @@ def process_frame(info):
 	newimage = info["frame"].quantize(palette=info["palette_img"], dither=True)
 	palette = newimage.getpalette() # TODO: I don't think this getpalette() call is necessary!
 	palette = info["palette"]
+	lowest_num = 1000
 	for y in range(info["height"]):
 		for x in range(modified_width):
-			print(newimage.getpixel((x, y)))
+			cur_num = newimage.getpixel((x, y))
+			if (cur_num < lowest_num):
+				lowest_num = cur_num
+				print(lowest_num)
 			string += char.get_char(info["f"], palette, newimage.getpixel((x, y)))
 		# the last character in a frame doesn't need a return character after it
 		if y < info["height"] - 1:
