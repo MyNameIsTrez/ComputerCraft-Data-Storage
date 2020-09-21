Fixed char.py issue in asciify, it had to do with how uint8 can't hold negative values and how multiplying with a number can easily cause an overflow with uint8's 255 max.

extra/Dithering with NumPy/utils/char_blue.py
@@ -3,15 +3,11 @@ import numpy as np
 
 
 palettes = {
-	"vanilla": [0, 0, 0, 13, 13, 13, 27, 27, 27, 40, 40, 40, 54, 54, 54, 67, 67, 67, 81, 81, 81, 94, 94, 94, 107, 107, 107, 121, 121, 121, 134, 134, 134, 148, 148, 148, 161, 161, 161, 174, 174, 174, 188, 188, 188, 201, 201, 201, 215, 215, 215, 228, 228, 228, 242, 242, 242, 255, 255, 255],
-
 	"color": [0, 0, 0, 0, 0, 255, 0, 255, 0, 0, 255, 255, 255, 0, 0, 255, 0, 255, 255, 255, 0, 255, 255, 255, 158, 23, 171, 67, 107, 74, 225, 123, 151, 55, 200, 50, 174, 75, 78, 9, 92, 209, 103, 171, 47, 22, 202, 138, 122, 198, 83, 114, 233, 135, 73, 135, 158, 244, 202, 9, 62, 171, 132, 213, 227, 77, 8, 145, 152, 86, 154, 244, 40, 1, 160, 87, 23, 106, 225, 145, 48, 52, 4, 25, 187, 77, 161, 90, 65, 107, 99, 127, 23, 232, 85, 250, 53, 93, 139, 205, 179, 38, 193, 219, 39, 97, 50, 63, 203, 19, 208, 251, 72, 212, 21, 3, 33, 182, 93, 98, 33, 193, 226, 56, 169, 55, 50, 222, 148, 190, 171, 71, 228, 108, 35, 11, 206, 85, 74, 153, 105, 27, 149, 177, 239, 170, 180, 17, 96, 18, 115, 129, 211, 85, 186, 124, 140, 236, 90, 32, 170, 5, 104, 142, 113, 139, 154, 63, 224, 228, 217, 73, 255, 110, 250, 239, 24, 175, 0, 120, 193, 115, 249, 148, 201, 179, 16, 85, 160, 106, 118, 74, 111, 222, 217, 224, 105, 115, 32, 73, 87, 249, 147, 87, 103, 141, 70, 127, 60, 70, 247, 253, 185, 240, 145, 130, 164, 161, 216, 68, 11, 78, 142, 122, 44, 103, 91, 149, 218, 123, 222, 50, 116, 113, 137, 219, 255, 207, 90, 59, 21, 81, 57, 206, 65, 202, 81, 229, 108, 41, 41, 119, 119, 184, 242, 163, 31, 77, 56, 250, 200, 22, 44, 60, 136, 181, 23, 135, 132, 113, 194, 153, 194, 251, 103, 3, 198, 184, 233, 50, 225, 18],
 }
 
 
 chars = {
-	"vanilla": [" ", ".", "'", ":", "-", "!", "/", "(", "=", "%", "1", "C", "3", "$", "2", "5", "A", "0", "#", "@"],
-
 	"color": [' ', '!', '\\"', '#', '$', '%', '&', "\\'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\\\', ']', '^', '_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~'],
 }
 
@@ -71,46 +67,29 @@ def add_err(pxls, err, coeff, x, y, w, h):
 	pxls[y, x] = np.rint(np.clip(pxls[y, x] + err / 16 * coeff, 0, 255))
 
 
-
-def get_char(pal_name, char_idx):
-	return chars[pal_name][char_idx]
-
-
-# TODO: Increase speed with pxls as a NP array, look up "python pil replace a single rgba color" on StackOverflow for it
-def dither_to_str(info):
-	# TODO: Compare performance with a list, because Python may be recreating the string after every +=
-	string = ""
-
-	# the \n character at the end of every line needs to have one spot reserved
-	# TODO: do the -1 at the resizing of the frame instead
-	# Is it really necessary to use ["new_width"] instead of ["frame"].width?
+def dither_frame(info):
 	modified_width = info["new_width"] - 1
 
 	pal_name = info["palette"]
 	pal = palettes[pal_name]
 	chars_count = len(chars[pal_name])
 
-	# TODO: Try to remove the list() call
-	# pxls = list(info["frame"].getdata())
-	# TODO: np.array call seems pretty slow with 0.08s as opposed to 0.01s with list()
 	pxls = np.array(info["frame"])
 
 	w = info["frame"].width
 	h = info["frame"].height
 
+	dithered_frame = Image.new("RGBA", (modified_width, info["height"]))
+	dithered_frame_pxls = np.array(dithered_frame)
+
 	for y in range(info["height"]):
-		for x in range(modified_width): #TODO: modified_width be gone
+		for x in range(modified_width):
 			cur_clr = pxls[y, x]
 
 			closest_pal_clr, char_idx = get_closest_pal(cur_clr, pal, chars_count)
 
-			string += get_char(pal_name, char_idx)
+			dithered_frame.putpixel((x, y), tuple(closest_pal_clr))
 
 			distribute_err(pxls, cur_clr, closest_pal_clr, x, y, w, h)
 
-		# TODO: why not y < h - 1?
-		if y < info["height"] - 1:
-			# add a return character to the end of each horizontal line,
-			# so ComputerCraft can draw the entire frame with one write() statement
-			string += "\\n"
-	return string
+	info["dithered_frame"] = dithered_frame

extra/Dithering with NumPy/utils/char_copy.py
@@ -15,23 +15,20 @@ chars = {
 def get_closest_pal(cur_clr, pal, chars_count):
 	smallest_dist = float("inf")
 
-	cur_r = cur_clr[0]
-	cur_g = cur_clr[1]
-	cur_b = cur_clr[2]
-
+	(cur_r, cur_g, cur_b, cur_a) = cur_clr
 	# print("cur_r: {}, cur_g: {}, cur_b: {}".format(cur_r, cur_g, cur_b))
 	
 	for char_idx in range(chars_count):
+		# print("\nchar_idx: {}".format(char_idx))
+
 		pal_r = pal[char_idx * 3]
 		pal_g = pal[char_idx * 3 + 1]
 		pal_b = pal[char_idx * 3 + 2]
-		
-		# print("\npal_r: {}, pal_g: {}, pal_b: {}".format(pal_r, pal_g, pal_b))
+		# print("pal_r: {}, pal_g: {}, pal_b: {}".format(pal_r, pal_g, pal_b))
 
 		r_diff = cur_r - pal_r
 		g_diff = cur_g - pal_g
 		b_diff = cur_b - pal_b
-
 		# print("r_diff: {}, g_diff: {}, b_diff: {}".format(r_diff, g_diff, b_diff))
 
 		avg_r = (cur_r + pal_r) / 2
@@ -47,17 +44,24 @@ def get_closest_pal(cur_clr, pal, chars_count):
 		
 		if dist < smallest_dist:
 			smallest_dist = dist
-			if (len(cur_clr) == 3):
-				closest_pal_clr = (pal_r, pal_g, pal_b)
-			else:
-				closest_pal_clr = (pal_r, pal_g, pal_b, 255)
 			closest_char_idx = char_idx
+			
+			if (len(cur_clr) == 3):
+				# closest_pal_clr = np.array([pal_r, pal_g, pal_b]).astype(np.uint8)
+				closest_pal_clr = np.array([pal_r, pal_g, pal_b])
+			else:
+				# closest_pal_clr = np.array([pal_r, pal_g, pal_b, 255]).astype(np.uint8)
+				closest_pal_clr = np.array([pal_r, pal_g, pal_b, 255])
 
 	return closest_pal_clr, closest_char_idx
 
 
 def distribute_err(pxls, cur_clr, closest_pal_clr, x, y, w, h):
-	err = np.subtract(cur_clr, closest_pal_clr)
+	# print("cur_clr dtype: {}".format(cur_clr.dtype))
+	# print("closest_pal_clr dtype: {}".format(closest_pal_clr.dtype))
+	# err = np.subtract(cur_clr, closest_pal_clr)
+	err = cur_clr - closest_pal_clr
+	# print("err dtype: {}".format(err.dtype))
 	
 	add_err(pxls, err, 7, x + 1, y    , w, h)
 	add_err(pxls, err, 3, x - 1, y + 1, w, h)
@@ -69,9 +73,12 @@ def add_err(pxls, err, coeff, x, y, w, h):
 	if x < 0 or x >= w or y < 0 or y >= h:
 		return
 	
-	cur_clr = pxls[y, x]
-	new_clr = cur_clr + err * coeff / 16
-	pxls[y, x] = new_clr
+	# numpy.core._exceptions.UFuncTypeError: Cannot cast ufunc 'add' output from dtype('float64') to dtype('uint8') with casting rule 'same_kind'
+	# pxls[y, x] += err * coeff / 16
+
+	pxls[y, x] = pxls[y, x] + err * int(coeff / 16)
+	# print("pxls[y, x] dtype: {}".format(pxls[y, x].dtype))
+	# print("err dtype: {}".format(err.dtype))
 
 
 def dither_frame(info):
@@ -92,13 +99,14 @@ def dither_frame(info):
 	for y in range(info["height"]):
 		for x in range(modified_width):
 			# print("\n\n\nx :{}, y: {}".format(x, y))
+
 			cur_clr = pxls[y, x]
-
-			# print("foo")
-			closest_pal_clr, char_idx = get_closest_pal(cur_clr, pal, chars_count)
 			# print("cur_clr: {}".format(cur_clr))
+			
+			closest_pal_clr, char_idx = get_closest_pal(cur_clr, pal, chars_count)
+			# print("foo")
 
-			dithered_frame.putpixel((x, y), closest_pal_clr)
+			dithered_frame.putpixel((x, y), tuple(closest_pal_clr))
 
 			distribute_err(pxls, cur_clr, closest_pal_clr, x, y, w, h)
 