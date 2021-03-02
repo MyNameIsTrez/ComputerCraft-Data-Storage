0b79a1188db0db4de5f203b0abcffaeb57516aab
MyNameIsTrez <welfje@gmail.com>
2021-03-02 19:17:17 +0100

    Working on stuff that's buggy.

--- extra/palette-generator/efficient-closing-2d/main.js
+++ extra/palette-generator/efficient-closing-2d/main.js
@@ -3,7 +3,7 @@
 // NOT USER SETTINGS //
 
 let w, h, r;
-let gridSize, maxDistSq;
+let gridSize, rSq;
 
 let leftOpenIndexes, rightOpenIndexes;
 
@@ -13,19 +13,19 @@ let allTestsPassed = true;
 //// USED IN DRAWCIRCLES() ////
 
 const framesBetweenPlacement = 1; // 1 is max speed.
-const placeSpeedMultiplier = 10; // 10
+const placeSpeedMultiplier = 100; // 10
 const drawing = true;
 const drawSquares = true; // If false it'll draw circles.
-const printPlacementStats = false;
+const printPlacementStats = true;
 
 const colors = [
-    [50, 50, 200],
-    [50, 200, 50],
-    [50, 200, 200],
-    [200, 50, 50],
-    [200, 50, 200],
-    [200, 200, 50],
-    [200, 200, 200],
+	[50, 50, 200],
+	[50, 200, 50],
+	[50, 200, 200],
+	[200, 50, 50],
+	[200, 50, 200],
+	[200, 200, 50],
+	[200, 200, 200],
 ];
 
 let placed = 0;
@@ -39,111 +39,112 @@ let colorIndex = 0;
 
 
 function initOpenIndexes() {
-    leftOpenIndexes = [];
-    rightOpenIndexes = [];
+	leftOpenIndexes = [];
+	rightOpenIndexes = [];
 
-    leftOpenIndexes[0] = 0;
-    rightOpenIndexes[0] = gridSize - 1;
+	leftOpenIndexes[0] = 0;
+	rightOpenIndexes[0] = gridSize - 1;
 }
 
 
 function getRandomOpenIndex() {
-    let nthOpenIndex = getRandomInt(getOpenIndexCount()) + 1;
+	let nthOpenIndex = getRandomInt(getOpenIndexCount()) + 1;
 
-    let openIndexesSeen = 0;
-    let difference, startOfLeft, offset;
+	let openIndexesSeen = 0;
+	let difference, startOfLeft, offset;
 
-    for (let index = 0; index < leftOpenIndexes.length; index++) {
-        difference = rightOpenIndexes[index] - leftOpenIndexes[index] + 1;
-        if (openIndexesSeen + difference >= nthOpenIndex) {
-            startOfLeft = leftOpenIndexes[index] - 1;
-            offset = nthOpenIndex - openIndexesSeen;
-            return startOfLeft + offset;
-        } else {
-            openIndexesSeen += difference;
-        }
-    }
+	for (let index = 0; index < leftOpenIndexes.length; index++) {
+		difference = rightOpenIndexes[index] - leftOpenIndexes[index] + 1;
+		if (openIndexesSeen + difference >= nthOpenIndex) {
+			startOfLeft = leftOpenIndexes[index] - 1;
+			offset = nthOpenIndex - openIndexesSeen;
+			return startOfLeft + offset;
+		} else {
+			openIndexesSeen += difference;
+		}
+	}
 }
 
 
  // TODO: Replace with code that continuously decrements openIndexCount instead for better time complexity.
 function getOpenIndexCount() {
-  let openIndexCount = 0;
-
-  for (let index = 0; index < leftOpenIndexes.length; index++) {
-    openIndexCount += rightOpenIndexes[index] - leftOpenIndexes[index] + 1;
-  }
-
-  return openIndexCount;
+	let openIndexCount = 0;
+	for (let index = 0; index < leftOpenIndexes.length; index++) {
+		openIndexCount += rightOpenIndexes[index] - leftOpenIndexes[index] + 1;
+	}
+	return openIndexCount;
 }
 
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+	return Math.floor(Math.random() * Math.floor(max));
 }
 
 
-function placeCircle(index, r, w, h, gridSize, maxDistSq) {
-  let xClosedLeft, xClosedRight;
-  let iClosedLeft, iClosedRight;
-  let iOpenLeft, iOpenRight;
+function placeCircle(index, r, w, h, gridSize, rSq) {
+	let xClosedLeft, xClosedRight;
+	let iClosedLeft, iClosedRight;
+	let iOpenLeft, iOpenRight;
 
-  let newLeftOpenIndexes = [];
-  let newRightOpenIndexes = [];
+	let newLeftOpenIndexes = [];
+	let newRightOpenIndexes = [];
 
-  const mx = index % w;
-  const my = Math.floor(index / w);
+	const mx = index % w;
+	const my = Math.floor(index / w);
 
-  let yDiffSq;
+	let yDiffSq;
 
-  if (getFirstIClosedLeft(my, r, h, mx, maxDistSq, w) != 0) {
-    newLeftOpenIndexes.push(0);
-  }
+	if (getFirstIClosedLeft(my, r, h, mx, rSq, w) != 0) {
+		newLeftOpenIndexes.push(0);
+	}
 
-  for (let y = Math.max(0, my - r); y < Math.min(h, my + r + 1); y++) {
-    yDiffSq = (my - y) ** 2;
+	for (let y = Math.max(0, my - r); y < Math.min(h, my + r + 1); y++) {
+		yDiffSq = (my - y) ** 2;
 
-    xClosedLeft = getXClosedLeft(mx, r, yDiffSq, maxDistSq);
-    xClosedRight = getXClosedRight(h, mx, r, yDiffSq, maxDistSq);
+		xClosedLeft = getXClosedLeft(mx, r, yDiffSq, rSq);
+		xClosedRight = getXClosedRight(h, mx, r, yDiffSq, rSq);
+		
+		// xClosedLeft = getXClosedLeft(rSq, yDiffSq, mx);
+		// xClosedRight = getXClosedRight(h, mx, r, yDiffSq, rSq);
 
-    if (drawing) {
-        if (drawSquares) {
-            drawRect(xClosedLeft, y, xClosedRight + 1, y + 1, w, h);
-        } else {
-            const mxScaled = mx * (width / w);
-            const myScaled = my * (height / h);
-            const rScaled = r * width / w;
-            circle(mxScaled, myScaled, rScaled);
-        }
-    }
+		if (drawing) {
+			if (drawSquares) {
+				drawRect(xClosedLeft, y, xClosedRight + 1, y + 1, w, h);
+			} else {
+				const mxScaled = mx * (width / w);
+				const myScaled = my * (height / h);
+				const rScaled = r * width / w;
+				circle(mxScaled, myScaled, rScaled);
+			}
+		}
 
-    iClosedLeft = xClosedLeft + y * w;
-    iClosedRight = xClosedRight + y * w;
+		iClosedLeft = xClosedLeft + y * w;
+		iClosedRight = xClosedRight + y * w;
 
-    iOpenRight = iClosedLeft - 1;
-    if (iOpenRight >= 0) {
-      newRightOpenIndexes.push(iOpenRight);
-    }
+		iOpenRight = iClosedLeft - 1;
+		if (iOpenRight >= 0) {
+			newRightOpenIndexes.push(iOpenRight);
+		}
 
-    iOpenLeft = iClosedRight + 1;
-    if (iOpenLeft <= gridSize - 1) {
-      newLeftOpenIndexes.push(iOpenLeft);
-    }
-  }
+		iOpenLeft = iClosedRight + 1;
+		if (iOpenLeft <= gridSize - 1) {
+			newLeftOpenIndexes.push(iOpenLeft);
+		}
+	}
 
-  if (getLastIClosedRight(my, r, h, mx, maxDistSq, w) != gridSize - 1) {
-    newRightOpenIndexes.push(gridSize - 1);
-  }
+	if (getLastIClosedRight(my, r, h, mx, rSq, w) != gridSize - 1) {
+		newRightOpenIndexes.push(gridSize - 1);
+	}
 
-  combineOpenIndexes(newLeftOpenIndexes, newRightOpenIndexes, w, h, gridSize);
+	combineOpenIndexes(newLeftOpenIndexes, newRightOpenIndexes, w, h, gridSize);
 }
 
 
-function getFirstIClosedLeft(my, r, h, mx, maxDistSq, w) {
+function getFirstIClosedLeft(my, r, h, mx, rSq, w) {
   if (Math.max(0, my - r) < Math.min(h, my + r + 1)) {
     const firstY = Math.max(0, my - r);
     const yDiffSq = (my - firstY) ** 2;
-    const xClosedLeft = getXClosedLeft(mx, r, yDiffSq, maxDistSq);
+    const xClosedLeft = getXClosedLeft(mx, r, yDiffSq, rSq);
     const iClosedLeft = xToIndex(xClosedLeft, firstY, w);
     console.log(iClosedLeft);
     // console.log(y, yDiffSq, xClosedLeft, iClosedLeft);
@@ -152,23 +153,34 @@ function getFirstIClosedLeft(my, r, h, mx, maxDistSq, w) {
 }
 
 
-function getLastIClosedRight(my, r, h, mx, maxDistSq, w) {
-  if (Math.max(0, my - r) < Math.min(h, my + r + 1)) {
-    const lastY = Math.min(h, my + r + 1) - 1;
-    const yDiffSq = (my - lastY) ** 2;
-    const xClosedRight = getXClosedRight(h, mx, r, yDiffSq, maxDistSq);
-    const iClosedRight = xToIndex(xClosedRight, lastY, w);
-    return iClosedRight;
-  }
+// function getFirstIClosedLeft(my, r, h, mx, rSq, w, yOffsetSq) {
+// 	if (Math.max(0, my - r) < Math.min(h, my + r + 1)) {
+// 		const yOffsetSq = Math.min(my, r) ** 2;
+// 		const xClosedLeft = getXClosedLeft(rSq, yOffsetSq, mx);
+// 		return xToIndex(xClosedLeft, yOffsetSq, w);
+// 	}
+// }
+
+
+function getLastIClosedRight(my, r, h, mx, rSq, w) {
+	if (Math.max(0, my - r) < Math.min(h, my + r + 1)) {
+		// const yOffsetSq = Math.min(h - my, r) ** 2;
+
+		const lastY = Math.min(h, my + r + 1) - 1;
+		const yOffsetSq = (my - lastY) ** 2;
+		const xClosedRight = getXClosedRight(h, mx, r, yOffsetSq, rSq);
+		const iClosedRight = xToIndex(xClosedRight, lastY, w);
+		return iClosedRight;
+	}
 }
 
 
 function xToIndex(x, y, w) {
-  return x + y * w;
+	return x + y * w;
 }
 
 
-function getXClosedLeft(mx, r, yDiffSq, maxDistSq) {
+function getXClosedLeft(mx, r, yDiffSq, rSq) {
   let xLeft, distLeftSq;
 
   xLeft = Math.max(0, mx - r);
@@ -177,7 +189,7 @@ function getXClosedLeft(mx, r, yDiffSq, maxDistSq) {
   // 
   distLeftSq = (mx - xLeft) ** 2 + yDiffSq;
 
-  while (distLeftSq > maxDistSq) { // TODO: May loop infinitely due to my - y?
+  while (distLeftSq > rSq) { // TODO: May loop infinitely due to my - y?
     xLeft++;
     distLeftSq = (mx - xLeft) ** 2 + yDiffSq;
   }
@@ -186,19 +198,33 @@ function getXClosedLeft(mx, r, yDiffSq, maxDistSq) {
 }
 
 
-function getXClosedRight(h, mx, r, yDiffSq, maxDistSq) {
-  let xRight, distRightSq;
+// function getXClosedLeft(rSq, yOffsetSq, mx) {
+// 	// Circle: (xOffset - mx)^2 + (yOffset - my)^2 = r^2
+// 	// Unit circle: xOffset^2 + yOffset^2 = r^2
+// 	// xOffset^2 = rSq - yOffsetSq
+// 	// xOffset = sqrt(rSq - yOffsetSq)
+// 	const xOffset = Math.sqrt(rSq - yOffsetSq);
+// 	return Math.max(0, mx - xOffset);
+// }
 
-  xRight = Math.min(h - 1, mx + r);
+// function getXClosedRight(rSq, yOffsetSq, mx) {
 
-  distRightSq = (xRight - mx) ** 2 + yDiffSq;
+// }
 
-  while (distRightSq > maxDistSq) {
-    xRight--;
-    distRightSq = (xRight - mx) ** 2 + yDiffSq;
-  }
 
-  return xRight;
+function getXClosedRight(h, mx, r, yDiffSq, rSq) {
+	let xRight, distRightSq;
+
+	xRight = Math.min(h - 1, mx + r);
+
+	distRightSq = (xRight - mx) ** 2 + yDiffSq;
+
+	while (distRightSq > rSq) {
+		xRight--;
+		distRightSq = (xRight - mx) ** 2 + yDiffSq;
+	}
+
+	return xRight;
 }
 
 
@@ -211,179 +237,177 @@ function getXClosedRight(h, mx, r, yDiffSq, maxDistSq) {
  ** [ [5, 6], [9,10] ] + [ [2,3], [6, 7] ] = [     [6,6]     ]
  */
 function combineOpenIndexes(newLeftOpenIndexes, newRightOpenIndexes, w, h, gridSize) {
-  let oldLeftOpenIndexes = [...leftOpenIndexes];
-  let oldRightOpenIndexes = [...rightOpenIndexes];
+	let oldLeftOpenIndexes = [...leftOpenIndexes];
+	let oldRightOpenIndexes = [...rightOpenIndexes];
 
-  const oldOpenIndexesLength = oldLeftOpenIndexes.length;
-  const newOpenIndexesLength = newLeftOpenIndexes.length;
+	const oldOpenIndexesLength = oldLeftOpenIndexes.length;
+	const newOpenIndexesLength = newLeftOpenIndexes.length;
 
-  let oldIndex;
+	let oldIndex;
 
-  let newLeftIndex, newRightIndex, newLeftIndexNext;
+	let newLeftIndex, newRightIndex, newLeftIndexNext;
 
-  let start, end;
+	let start, end;
 
-  let combinedLeftOpenIndexes = []; // Max indexes can be calculated and used so this array can be recycled in C.
-  let combinedRightOpenIndexes = [];
+	let combinedLeftOpenIndexes = []; // Max indexes can be calculated and used so this array can be recycled in C.
+	let combinedRightOpenIndexes = [];
 
-  let closestIndex;
+	// TODO: Look into ropes (binary tree structure) as faster algorithm.
 
-  // TODO: Look into ropes (binary tree structure) as faster algorithm.
+	for (let newIndex = 0; newIndex < newOpenIndexesLength; newIndex++) {
+		oldIndex = binarySearchClosest(newLeftOpenIndexes[newIndex], newRightOpenIndexes[newIndex], oldLeftOpenIndexes, oldRightOpenIndexes);
 
-  for (let newIndex = 0; newIndex < newOpenIndexesLength; newIndex++) {
-    oldIndex = binarySearchClosest(newLeftOpenIndexes[newIndex], newRightOpenIndexes[newIndex], oldLeftOpenIndexes, oldRightOpenIndexes);
+		// console.log(`newLeftOpenIndexes[newIndex]: ${newLeftOpenIndexes[newIndex]}, newRightOpenIndexes[newIndex]: ${newRightOpenIndexes[newIndex]}, oldLeftOpenIndexes: ${oldLeftOpenIndexes}, oldRightOpenIndexes: ${oldRightOpenIndexes}`);
+		// console.log(`oldIndex: ${oldIndex}`);
 
-    // console.log(`newLeftOpenIndexes[newIndex]: ${newLeftOpenIndexes[newIndex]}, newRightOpenIndexes[newIndex]: ${newRightOpenIndexes[newIndex]}, oldLeftOpenIndexes: ${oldLeftOpenIndexes}, oldRightOpenIndexes: ${oldRightOpenIndexes}`);
-    // console.log(`oldIndex: ${oldIndex}`);
+		if (oldIndex == -1) continue; // Error handling may be unnecessary.
 
-    if (oldIndex == -1) continue; // Error handling may be unnecessary.
+		newLeftIndex = newLeftOpenIndexes[newIndex];
+		newRightIndex = newRightOpenIndexes[newIndex];
 
-    newLeftIndex = newLeftOpenIndexes[newIndex];
-    newRightIndex = newRightOpenIndexes[newIndex];
+		newLeftIndexNext = newLeftOpenIndexes[newIndex + 1];
 
-    newLeftIndexNext = newLeftOpenIndexes[newIndex + 1];
+		while (oldIndex < oldOpenIndexesLength) {
+			start = Math.max(oldLeftOpenIndexes[oldIndex], newLeftIndex);
+			end = Math.min(oldRightOpenIndexes[oldIndex], newRightIndex);
 
-    while (oldIndex < oldOpenIndexesLength) {
-      start = Math.max(oldLeftOpenIndexes[oldIndex], newLeftIndex);
-      end = Math.min(oldRightOpenIndexes[oldIndex], newRightIndex);
+			if (end >= start) {
+				combinedLeftOpenIndexes.push(start);
+				combinedRightOpenIndexes.push(end);
+			}
 
-      if (end >= start) {
-        combinedLeftOpenIndexes.push(start);
-        combinedRightOpenIndexes.push(end);
-      }
+			if (newIndex + 1 <= newOpenIndexesLength && newLeftIndexNext <= oldRightOpenIndexes[oldIndex]) {
+				// newIndex++;
+				break;
+			} else {
+				oldIndex++;
+			}
+		}
+	}
 
-      if (newIndex + 1 <= newOpenIndexesLength && newLeftIndexNext <= oldRightOpenIndexes[oldIndex]) {
-        // newIndex++;
-        break;
-      } else {
-        oldIndex++;
-      }
-    }
-  }
+	leftOpenIndexes = [...combinedLeftOpenIndexes];
+	rightOpenIndexes = [...combinedRightOpenIndexes];
 
-  leftOpenIndexes = [...combinedLeftOpenIndexes];
-  rightOpenIndexes = [...combinedRightOpenIndexes];
-
-  // console.log(leftOpenIndexes, rightOpenIndexes);
-  // console.log();
+	// console.log(leftOpenIndexes, rightOpenIndexes);
+	// console.log();
 }
 
 
 
 function binarySearchClosest(newLeft, newRight, oldLeftArr, oldRightArr) {
-  const value = newLeft;
+	const value = newLeft;
 
-  if (newRight < oldLeftArr[0]) return -1; // -1 means that it should be skipped, might be an unnecessary check.
-  if (newLeft > oldRightArr[oldRightArr.length - 1]) return -1;
+	if (newRight < oldLeftArr[0]) return -1; // -1 means that it should be skipped, might be an unnecessary check.
+	if (newLeft > oldRightArr[oldRightArr.length - 1]) return -1;
 
-  // TODO: In C this function could just use ints here instead of lets so the Math.floor below can be removed.
-  let low = 0;
-  let high = oldLeftArr.length - 1;
-  let mid;
+	// TODO: In C this function could just use ints here instead of lets so the Math.floor below can be removed.
+	let low = 0;
+	let high = oldLeftArr.length - 1;
+	let mid;
 
-  while (low <= high) {
-    mid = Math.floor((high + low) / 2);
-    // console.log(`low: ${low}, mid: ${mid}, high: ${high}, value: ${value}, oldLeftArr[mid]: ${oldLeftArr[mid]}, value < oldLeftArr[mid]: ${value < oldLeftArr[mid]}`);
+	while (low <= high) {
+		mid = Math.floor((high + low) / 2);
+		// console.log(`low: ${low}, mid: ${mid}, high: ${high}, value: ${value}, oldLeftArr[mid]: ${oldLeftArr[mid]}, value < oldLeftArr[mid]: ${value < oldLeftArr[mid]}`);
 
-    if (value < oldLeftArr[mid]) {
-      high = mid - 1;
-    } else if (value > oldLeftArr[mid]) {
-      low = mid + 1;
-    } else {
-      // console.log("return 1");
-      return mid;
-    }
-  }
-  // console.log("return 2");
-  // console.log(`low: ${low}, high: ${high}`);
-  return Math.max(0, high); // This probably won't work in all cases.
-  // return (oldLeftArr[low] - value) < (value - oldLeftArr[high]) ? low : high;
+		if (value < oldLeftArr[mid]) {
+			high = mid - 1;
+		} else if (value > oldLeftArr[mid]) {
+			low = mid + 1;
+		} else {
+			// console.log("return 1");
+			return mid;
+		}
+	}
+	// console.log("return 2");
+	// console.log(`low: ${low}, high: ${high}`);
+	return Math.max(0, high); // This probably won't work in all cases.
+	// return (oldLeftArr[low] - value) < (value - oldLeftArr[high]) ? low : high;
 }
 
 
 function initDrawGrid() {
-  const sideLength = Math.min(innerWidth - 1, innerHeight - 1);
-  createCanvas(sideLength, sideLength);
+	const sideLength = Math.min(innerWidth - 1, innerHeight - 1);
+	createCanvas(sideLength, sideLength);
 
-  // rectMode(RADIUS);
-  rectMode(CORNERS);
-  textAlign(CENTER, CENTER);
-  // ellipseMode(RADIUS);
+	// rectMode(RADIUS);
+	rectMode(CORNERS);
+	textAlign(CENTER, CENTER);
+	// ellipseMode(RADIUS);
 
-  noSmooth();
+	noSmooth();
 
-  stroke(175, 50, 50);
-  fill(175, 50, 50);
+	stroke(175, 50, 50);
+	fill(175, 50, 50);
 }
 
 
 function drawRect(x1, y1, x2, y2, w, h) {
-    const widthMult = width / w;
-    const heightMult = height / h;
-    
-    const x1_ = x1 * widthMult;
-    const y1_ = y1 * heightMult;
-    const x2_ = x2 * widthMult;
-    const y2_ = y2 * heightMult;
-    rect(x1_, y1_, x2_, y2_);
+	const widthMult = width / w;
+	const heightMult = height / h;
+	
+	const x1_ = x1 * widthMult;
+	const y1_ = y1 * heightMult;
+	const x2_ = x2 * widthMult;
+	const y2_ = y2 * heightMult;
+	rect(x1_, y1_, x2_, y2_);
 }
 
 
 function setup() {
-    initDrawGrid();
-    runTests();
+	initDrawGrid();
+	runTests();
 
-    // USER SETTINGS //
-    
-    w = 256; // Width of grid.
-    h = 256; // Height of grid.
-    r = 1; // Radius of circle, 0 means a single 1x1 cell.
+	// USER SETTINGS //
+	
+	w = 256; // Width of grid.
+	h = 256; // Height of grid.
+	r = 1; // Radius of circle, 0 means a single 1x1 cell.
 
-    gridSize = w * h;
-    maxDistSq = r ** 2;
+	gridSize = w * h;
+	rSq = r ** 2;
 
-    ///////////////////
+	///////////////////
 
-    initOpenIndexes();
+	initOpenIndexes();
 }
 
 
 function draw() {
-    if (allTestsPassed) {
-        drawCircles();
-    }
+	if (allTestsPassed) {
+		drawCircles();
+	}
 }
 
 
-function drawCircles() {    
-    let chosenColor;
-    let index;
-    
-    if (frameCount % framesBetweenPlacement == 0) {
-        chosenColor = colors[colorIndex++ % colors.length];
-        fill(chosenColor);
-        stroke(chosenColor);
-    
-        for (let i = 0; i < placeSpeedMultiplier; i++) {
-            if (leftOpenIndexes.length != 0) {
-                index = getRandomOpenIndex();
-                placeCircle(index, r, w, h, gridSize, maxDistSq);
-                placed++;
-            }
-        }
-    }
-    
-    if (frameCount % 60 == 0) {
-        const open = leftOpenIndexes.length;
-        const placedPerSec = Math.floor(placed / ((performance.now() - prevTime) / 1000));
-        
-        if (printPlacementStats) {
-            console.log(`open: ${open} (${open-prevOpen}), placed/second: ${placedPerSec} (${placedPerSec-prevPlacedPerSec})`);
-        }
+function drawCircles() {		
+	let chosenColor;
+	let index;
+	
+	if (frameCount % framesBetweenPlacement == 0) {
+		chosenColor = colors[colorIndex++ % colors.length];
+		fill(chosenColor);
+		stroke(chosenColor);
+	
+		for (let i = 0; i < placeSpeedMultiplier; i++) {
+			if (leftOpenIndexes.length != 0) {
+				index = getRandomOpenIndex();
+				placeCircle(index, r, w, h, gridSize, rSq);
+				placed++;
+			}
+		}
+	}
+	
+	if (frameCount % 60 == 0) {
+		const open = leftOpenIndexes.length;
+		const placedPerSec = Math.floor(placed / ((performance.now() - prevTime) / 1000));
+		
+		if (printPlacementStats) {
+			console.log(`open: ${open} (${open-prevOpen}), placed/second: ${placedPerSec} (${placedPerSec-prevPlacedPerSec})`);
+		}
 
-        prevOpen = open;
-        prevPlacedPerSec = placedPerSec;
-        placed = 0;
-        prevTime = performance.now();
-    }
+		prevOpen = open;
+		prevPlacedPerSec = placedPerSec;
+		placed = 0;
+		prevTime = performance.now();
+	}
 }
--- extra/palette-generator/efficient-closing-2d/test.js
+++ extra/palette-generator/efficient-closing-2d/test.js
@@ -1,118 +1,118 @@
 function runTests() {
-    w = 5;
-    h = 5;
-    r = 1;
-    
-    gridSize = w * h;
-    maxDistSq = r ** 2;
-
-    
-    testOne(2, [], []);
+	w = 5;
+	h = 5;
+	r = 1;
+	
+	gridSize = w * h;
+	rSq = r ** 2;
 
 
-    // INDIVIDUAL //
-
-    // Corners.
-    testOne(0, [2, 6], [4, 24]); // 1
-    testOne(4, [0, 5, 10], [2, 8, 24]); // 2
-    testOne(20, [0, 16, 22], [14, 19, 24]); // 3
-    testOne(24, [0, 20], [18, 22]); // 4
-    
-    // Middle.
-    testOne(12, [0, 8, 14, 18], [6, 10, 16, 24]); // 5
+	// testOne(2, [], []);
 
 
-    // COMBINED //
-    
-    if (allTestsPassed) {
-        clear();
-        initOpenIndexes();
-    }
+	// INDIVIDUAL //
 
-    // Corners.
-    
-    test(0, [2, 6], [4, 24]); // 6
-    test(4, [2, 6, 10], [2, 8, 24]); // 7
-    test(20, [2, 6, 10, 16, 22], [2, 8, 14, 19, 24]); // 8
-    test(24, [2, 6, 10, 16, 22], [2, 8, 14, 18, 22]); // 9
-
-    // Middle.
-    test(12, [2, 6, 8, 10, 14, 16, 18, 22], [2, 6, 8, 10, 14, 16, 18, 22]); // 10
+	// Corners.
+	testOne(0, [2, 6], [4, 24]); // 1
+	testOne(4, [0, 5, 10], [2, 8, 24]); // 2
+	testOne(20, [0, 16, 22], [14, 19, 24]); // 3
+	testOne(24, [0, 20], [18, 22]); // 4
+	
+	// Middle.
+	testOne(12, [0, 8, 14, 18], [6, 10, 16, 24]); // 5
 
 
-    // TEMPLATE //
+	// COMBINED //
+	
+	if (allTestsPassed) {
+		clear();
+		initOpenIndexes();
+	}
 
-    // test(, [], []);
+	// Corners.
+	
+	test(0, [2, 6], [4, 24]); // 6
+	test(4, [2, 6, 10], [2, 8, 24]); // 7
+	test(20, [2, 6, 10, 16, 22], [2, 8, 14, 19, 24]); // 8
+	test(24, [2, 6, 10, 16, 22], [2, 8, 14, 18, 22]); // 9
+
+	// Middle.
+	test(12, [2, 6, 8, 10, 14, 16, 18, 22], [2, 6, 8, 10, 14, 16, 18, 22]); // 10
 
 
-    if (allTestsPassed) {
-        console.log("All tests passed! 🎉");
-        clear();
-    }
+	// TEMPLATE //
+
+	// test(, [], []);
+
+
+	if (allTestsPassed) {
+		console.log("All tests passed! 🎉");
+		clear();
+	}
 }
 
 
 function testOne(index, expectedLeftOpenIndexes, expectedRightOpenIndexes) {
-    if (allTestsPassed) {
-        clear();
-        initOpenIndexes();
-        test(index, expectedLeftOpenIndexes, expectedRightOpenIndexes);
-    }
+	if (allTestsPassed) {
+		clear();
+		initOpenIndexes();
+		test(index, expectedLeftOpenIndexes, expectedRightOpenIndexes);
+	}
 }
 
 
 function test(index, expectedLeftOpenIndexes, expectedRightOpenIndexes) {
-    if (allTestsPassed) {
-        placeCircle(index, r, w, h, gridSize, maxDistSq);
-        drawIndexesText();
-        testNumber++;
-        assertTest(leftOpenIndexes, expectedLeftOpenIndexes, rightOpenIndexes, expectedRightOpenIndexes);
-    }
+	if (allTestsPassed) {
+		placeCircle(index, r, w, h, gridSize, rSq);
+		drawIndexesText();
+		testNumber++;
+		assertTest(leftOpenIndexes, expectedLeftOpenIndexes, rightOpenIndexes, expectedRightOpenIndexes);
+	}
 }
 
 
 function drawIndexesText() {
-    for (let index = 0; index < gridSize; index++) {
-        drawCenteredText(index);
-    }
+	for (let index = 0; index < gridSize; index++) {
+		drawCenteredText(index);
+	}
 }
 
 
 function drawCenteredText(index) {
-    const mx = index % w;
-    const my = Math.floor(index / w);
-    const widthMult = width / w;
-    const heightMult = height / h;
-    const x = (mx + 0.5) * widthMult;
-    const y = (my + 0.5) * heightMult;
+	const mx = index % w;
+	const my = Math.floor(index / w);
+	const widthMult = width / w;
+	const heightMult = height / h;
+	const x = (mx + 0.5) * widthMult;
+	const y = (my + 0.5) * heightMult;
 
-    push();
-    noStroke();
-    fill(255);
-    text(index, x, y);
-    pop();
+	push();
+	noStroke();
+	fill(255);
+	text(index, x, y);
+	pop();
 }
 
 
 function arraysEqual(a1,a2) {
-    /* WARNING: Arrays must not contain {objects} or behavior may be undefined. */
-    return JSON.stringify(a1) === JSON.stringify(a2);
+	/* WARNING: Arrays must not contain {objects} or behavior may be undefined. */
+	return JSON.stringify(a1) === JSON.stringify(a2);
 }
 
 
 function assertTest(leftOpenIndexes, expectedLeftOpenIndexes, rightOpenIndexes, expectedRightOpenIndexes) {
-    const leftEqual = arraysEqual(leftOpenIndexes, expectedLeftOpenIndexes);
-    const rightEqual = arraysEqual(rightOpenIndexes, expectedRightOpenIndexes);
+	const leftEqual = arraysEqual(leftOpenIndexes, expectedLeftOpenIndexes);
+	const rightEqual = arraysEqual(rightOpenIndexes, expectedRightOpenIndexes);
 
-    if (!leftEqual || !rightEqual) {
-        allTestsPassed = false;
+	if (!leftEqual || !rightEqual) {
+		allTestsPassed = false;
 
-        console.log(`Test ${testNumber} failed!`);
+		console.log(`Test ${testNumber} failed!`);
 
-        console.log(`\nexpected left: ${expectedLeftOpenIndexes}`);
-        console.log(`expected right: ${expectedRightOpenIndexes}`);
-        
-        console.log(`\nleft: ${leftOpenIndexes}`);
-        console.log(`right: ${rightOpenIndexes}`);
-    }
+		console.log(`\nexpected left: ${expectedLeftOpenIndexes}`);
+		console.log(`expected right: ${expectedRightOpenIndexes}`);
+		
+		console.log(`\nleft: ${leftOpenIndexes}`);
+		console.log(`right: ${rightOpenIndexes}`);
+	}
 }
